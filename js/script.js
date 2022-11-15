aee6ec34067ee4a45701c3c8237b5f7fa1ba1cfc
Vanessa Zanotti <zanottivanessa11@gmail.com>
2022-11-14 23:57:49 -0300

    Feature - Inclusão de button back to top. Fix - Correção de algumas divergências no HTML e CSS.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initOutsideClick from './modules/outsideClick.js'
 import initMenuMobile from './modules/menu-mobile.js'
 import iniTabNav from './modules/tab-menu.js'
 import initModal from './modules/modal.js'
+import endOfTheSite from './modules/endOfTheSite.js'
 
 initOutsideClick()
 initMenuMobile()
 iniTabNav()
 initModal()
+endOfTheSite()
